bd5d6221b5dde2f3f6396f7eeee7ed52429c5894
Naman Agrawal <namanagrawal@Mac.lan>
2026-06-23 19:38:14 +0530

    Add rewrite from /home to root / to support Meta validation

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      {
+        source: '/home',
+        destination: '/',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
+
